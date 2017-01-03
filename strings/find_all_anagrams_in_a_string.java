2db4fd9d92b3e41ca68ee39b722b8804f4969ab6
Kalvin Huang <kalvin_huang@yahoo.com>
2017-01-02 22:54:29 -0800

    Add strings dif and move anagrams questions

--- strings/find_all_anagrams_in_a_string.java
+++ strings/find_all_anagrams_in_a_string.java
@@ -1,17 +1,12 @@
-package java_stff;
-import java.util.*;
-
 /*
     438. Find All Anagrams in a String
     ---
-    Example 1:
-
     Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
 
     Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
 
     The order of output does not matter.
-
+    ---
     Example 1:
 
     Input:
@@ -38,6 +33,8 @@ import java.util.*;
     The substring with start index = 1 is "ba", which is an anagram of "ab".
     The substring with start index = 2 is "ab", which is an anagram of "ab".
  */
+
+import java.util.*;
 
 public class findAnagram {
 	// Time Complexity: O(n)
