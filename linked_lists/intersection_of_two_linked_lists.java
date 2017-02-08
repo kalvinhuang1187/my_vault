ae0d8694eeee460c241e131ac88a200cc691b6e1
Kalvin Huang <kalvin_huang@yahoo.com>
2017-02-08 09:31:10 -0800

    Fix overflow styling to fit in 1 page

--- linked_lists/intersection_of_two_linked_lists.java
+++ linked_lists/intersection_of_two_linked_lists.java
@@ -43,10 +43,12 @@ public class Solution {
         ListNode a = headA;
         ListNode b = headB;
         
-        // In the first iteration, we will reset the pointer of one linkedlist to the head of another linkedlist after it reaches the tail node
+        // In the first iteration, we will reset the pointer of one linkedlist to the head of
+        //    another linkedlist after it reaches the tail node
         // In the second iteration, we will move two pointers until they points to the same node
         // So if two linkedlist intersects, the meeting point in second iteration must be the intersection point. 
-        // If the two linked lists have no intersection at all, then the meeting pointer in second iteration must be the tail node of both lists, which is null
+        // If the two linked lists have no intersection at all, then the meeting pointer in second iteration
+        //    must be the tail node of both lists, which is null
         while (a != b) {
             if (a != null)
                 a = a.next;
--- strings/string_to_integer_atoi.java
+++ strings/string_to_integer_atoi.java
@@ -3,19 +3,30 @@
 ---
 Implement atoi to convert a string to an integer.
 
-Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
+Notes: It is intended for this problem to be specified vaguely (ie, no given input specs).
+You are responsible to gather all the input requirements up front.
 ---
 Requirements for atoi:
-The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+The function first discards as many whitespace characters as necessary until the first
+non-whitespace character is found. Then, starting from this character, takes an optional
+initial plus or minus sign followed by as many numerical digits as possible, and interprets
+them as a numerical value.
 
-The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+The string can contain additional characters after those that form the integral number,
+which are ignored and have no effect on the behavior of this function.
 
-If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+If the first sequence of non-whitespace characters in str is not a valid integral number,
+or if no such sequence exists because either str is empty or it contains only whitespace
+characters, no conversion is performed.
 
-If no valid conversion could be performed, a zero value is returned. If the correct value is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
+If no valid conversion could be performed, a zero value is returned. If the correct value is
+out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
 ---
 HINT:
-To deal with overflow, inspect the current number before multiplication. If the current number is greater than 214748364, we know it is going to overflow. On the other hand, if the current number is equal to 214748364, we know that it will overflow only when the current digit is greater than or equal to 8.
+To deal with overflow, inspect the current number before multiplication. If the current number
+is greater than 214748364, we know it is going to overflow. On the other hand, if the current
+number is equal to 214748364, we know that it will overflow only when the current digit is
+greater than or equal to 8.
  */
 
 public class Solution {
